3b4afbb783d9ef35406acbb22e74542bd84c1c68
Justin Yost <justin.yost@yostivanich.com>
2014-11-16 19:12:39 -0800

    Updated Basic Design to Standard Bootstrap
    References #8
    
    Signed-off-by: Justin Yost <justin.yost@yostivanich.com>

--- index.php
+++ index.php
@@ -3,58 +3,87 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Secret Santa</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="Secret Santa">
 
 		<!-- Le styles -->
-		<link href="./css/bootstrap.css" rel="stylesheet">
-		<style>
-		  body {
-		    padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-		  }
-		</style>
-		<link href="./css/bootstrap-responsive.css" rel="stylesheet">
+		<link href="./css/bootstrap.min.css" rel="stylesheet">
+		<link href="./css/bootstrap-theme.min.css" rel="stylesheet">
+		<link href="./css/custom.css" rel="stylesheet">
 
-		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
-			<script src="./js/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
   </head>
 
-  <body>
+  <body role="document">
 
-		<div class="navbar navbar-inverse navbar-fixed-top">
-			<div class="navbar-inner">
-				<div class="container">
-					<a class="brand" href="#">Secret Santa</a>
-				</div>
+		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="container">
+			  <div class="navbar-header">
+			    <a class="navbar-brand" href="#">Secret Santa</a>
+			  </div>
+			  <div id="navbar" class="navbar-collapse collapse">
+			  </div><!--/.nav-collapse -->
 			</div>
+		</nav> <!-- Nav -->
+
+		<div class="container theme-showcase" role="main">
+			<div class="page-header">
+				<h1>Secret Santa</h1>
+				<p>Add each person's name and email who is participating in the Secret Santa drawing. Select the suggested price and hit the send email button. Everyone will receive an email with who they have been randomly select to purchase a gift for.</p>
+			</div>
+
+			<form class="form-horizontal" role="form">
+
+				<div id="person1">
+					<div class="form-group">
+							<label for="name1" class="col-sm-2 control-label">Name</label>
+							<div class="col-sm-10">
+							<input type="text" class="form-control" id="name1" placeholder="Enter name">
+							</div>
+					</div>
+
+					<div class="form-group">
+							<label for="email1" class="col-sm-2 control-label">Email</label>
+							<div class="col-sm-10">
+								<input type="email" class="form-control" id="email1" placeholder="Enter email">
+							</div>
+					</div>
+
+					<div class="form-group">
+							<label for="wishlist1" class="col-sm-2 control-label">Wishlist</label>
+							<div class="col-sm-10">
+								<input type="text" class="form-control" id="wishlist1" placeholder="Enter wishlist or other details">
+							</div>
+					</div>
+				</div>
+
+
+				<div class="form-group">
+					<div>
+						<button type="submit" class="btn btn-default">
+							<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Add another person
+						</button>
+					</div>
+				</div>
+				<div class="form-group">
+					<div>
+						<button type="submit" class="btn btn-default">Randomize and Email Participants</button>
+					</div>
+				</div>
+
+			</form>
 		</div>
 
-    <div class="container">
-
-      <h1>Secret Santa</h1>
-      <p>Add each person's name and email who is participating in the Secret Santa drawing. Select the suggested price and hit the send email button. Everyone will receive an email with who they have been randomly select to purchase a gift for.</p>
-
-    </div> <!-- /container -->
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./js/jquery.js"></script>
-    <script src="./js/bootstrap-transition.js"></script>
-    <script src="./js/bootstrap-alert.js"></script>
-    <script src="./js/bootstrap-modal.js"></script>
-    <script src="./js/bootstrap-dropdown.js"></script>
-    <script src="./js/bootstrap-scrollspy.js"></script>
-    <script src="./js/bootstrap-tab.js"></script>
-    <script src="./js/bootstrap-tooltip.js"></script>
-    <script src="./js/bootstrap-popover.js"></script>
-    <script src="./js/bootstrap-button.js"></script>
-    <script src="./js/bootstrap-collapse.js"></script>
-    <script src="./js/bootstrap-carousel.js"></script>
-    <script src="./js/bootstrap-typeahead.js"></script>
-		<script type="text/javascript" src="./includes/js/secret_santa.js" charset="utf-8"></script>
+		<!-- javascript -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="./js/bootstrap.min.js"></script>
+		<script src="./js/secret_santa.js"></script>
 		<script type="text/javascript">
 			var _gaq = _gaq || [];
 			_gaq.push(['_setAccount', '<?php echo GOOGLE_ANALYTICS_ID; ?>']);
