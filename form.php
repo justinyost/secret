40a1953f7319d51532382ea05fc5c14e83808a35
Justin Yost <justin.yost@yostivanich.com>
2014-11-30 16:52:36 -0800

    Adds Basic Passing Unit Tests
    Signed-off-by: Justin Yost <justin.yost@yostivanich.com>

--- form.php
+++ form.php
@@ -10,16 +10,38 @@ class RandomizePeople {
 
 class SubmitForm {
 
-	public function call($postData) {
+	/**
+	 * calls the necessary logic to process the form, randomize the people and send
+	 * the emails
+	 *
+	 * @param  array  $postData array of post data from the form
+	 * @return boolean
+	 */
+	public function call(array $postData = array()) {
+		if (
+			!is_array($postData)
+			|| !array_key_exists('data', $postData)
+		) {
+			return false;
+		}
+
 		$people = ($postData['data']) ? $postData['data'] : null;
+
+		if (
+			!is_array($people)
+			|| !array_key_exists('Person', $people)
+		) {
+			return false;
+		}
 
 		// Person should contain: name/email/wishlist
 		foreach($people['Person'] as $person) {
-			var_dump($person);
+
 		}
 	}
 }
 
-
-$SubmitForm = new SubmitForm();
-$SubmitForm->call($_POST);
+if (isset($_POST) && !empty($_POST)) {
+	$SubmitForm = new SubmitForm();
+	$SubmitForm->call($_POST);
+}
